c56f1688112c2336ffef56255d1e7daa17090d9e
Guilherme S D <guilherme.sousa.domingos@gmail.com>
2015-11-03 18:02:21 -0200

    Ajuste arquivos para o build

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -15,4 +15,5 @@ angular.module('orcamentoApp')
       'Karma'
     ];
 
+    $scope.use= true;
   });
--- app/scripts/directives/nav.js
+++ app/scripts/directives/nav.js
@@ -12,7 +12,7 @@ angular.module('orcamentoApp')
       templateUrl: 'views/nav.html',
       restrict: 'E',
 	  replace:true,
-	  link: function(scope, element, attributes) {
+	  link: function(scope) {
 		scope.local = '';
 		scope.$on('$locationChangeSuccess', function() {
 			scope.local = $location.path();
